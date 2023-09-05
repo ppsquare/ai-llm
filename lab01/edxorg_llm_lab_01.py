93fe22362c74bce518cb7eb92e89128c9f2bc878
ppsquare <50252025+ppsquare@users.noreply.github.com>
2023-09-04 21:29:25 -0400

    Data set detail updated

--- lab01/edxorg_llm_lab_01.py
+++ lab01/edxorg_llm_lab_01.py
@@ -10,6 +10,20 @@
 # )
 # xsum_dataset
 
+# xsum data sets
+
+# features: ['gem_id', 'gem_parent_id', 'xsum_id', 'document', 'target', 'references'] datasets list
+
+# 1. train , num_rows: 23206
+# 2. validation, num_rows: 1117
+# 3. test , num_rows: 1166
+# 4. challenge_train_sample , num_rows: 500
+# 5. challenge_validation_sample, num_rows: 500
+# 6. challenge_test_backtranslation, num_rows: 500
+# 7. challenge_test_bfp_02, num_rows: 500
+# 8. challenge_test_bfp_05, num_rows: 500
+# 9. challenge_test_nopunc, num_rows: 500
+# 10. challenge_test_covid, num_rows: 401
 
 import datasets
 data = datasets.load_dataset('GEM/xsum')
