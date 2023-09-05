8580c14134d62ff2ad55094a72531980aeb10758
ppsquare <50252025+ppsquare@users.noreply.github.com>
2023-09-04 20:26:46 -0400

    Create lib_install.py

---
+++ lab01/lib_install.py
@@ -0,0 +1,11 @@
+# ERROR: pip's dependency resolver does not currently take into account all the packages that are installed
+# %pip install h5py
+# %pip install typing-extensions
+# %pip install wheel
+
+# Spefic version will ensure when share the code to other to work without issue
+# following prompt is only need one time execution
+# %pip install nlptest==1.1.0
+
+# To Test the above insall run the below prompt
+#import nlptest
