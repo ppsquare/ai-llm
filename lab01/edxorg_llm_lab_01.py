f2dfe832f3fb49f065f25c52f759bb6b31f828a9
ppsquare <50252025+ppsquare@users.noreply.github.com>
2023-09-04 21:09:23 -0400

    xsum data loaded without error

--- lab01/edxorg_llm_lab_01.py
+++ lab01/edxorg_llm_lab_01.py
@@ -1,11 +1,16 @@
-from transformers.integrations import version
-from datasets import load_dataset
-from transformers import pipeline
+# from transformers.integrations import version
+# from datasets import load_dataset
+# from transformers import pipeline
 
-xsum_dataset = load_dataset (
-    "xsum",
-    version="1.2.0",
-    #cache_dir=DA.paths.datasets
-    cache_dir="https://huggingface.co/datasets/xsum"
-)
-xsum_dataset
+# xsum_dataset = load_dataset (
+#     "xsum",
+#     version="1.2.0",
+#     #cache_dir=DA.paths.datasets
+#     cache_dir="https://huggingface.co/datasets/xsum"
+# )
+# xsum_dataset
+
+
+import datasets
+data = datasets.load_dataset('GEM/xsum')
+data
