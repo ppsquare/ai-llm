7306c1d500b7904f09617108c5b3eee525725310
ppsquare <50252025+ppsquare@users.noreply.github.com>
2023-09-04 20:25:36 -0400

    Create edxorg_llm_lab_01.py

---
+++ lab01/edxorg_llm_lab_01.py
@@ -0,0 +1,11 @@
+from transformers.integrations import version
+from datasets import load_dataset
+from transformers import pipeline
+
+xsum_dataset = load_dataset (
+    "xsum",
+    version="1.2.0",
+    #cache_dir=DA.paths.datasets
+    cache_dir="https://huggingface.co/datasets/xsum"
+)
+xsum_dataset
